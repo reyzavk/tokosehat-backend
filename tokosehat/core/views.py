9aee849edd2df993597378d03fe8b31e387bdfc2
Reyvel <reyza@reyvel.id>
2019-08-30 21:13:59 +0700

    add filter

--- tokosehat/core/views.py
+++ tokosehat/core/views.py
@@ -4,17 +4,41 @@ from django.db.models import (
     OuterRef,
     Prefetch,
     Count,
+    Sum,
+    F,
 )
+from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.decorators import action
 from tokosehat.core import models, serializers
+from django_filters import rest_framework as filters
 # Create your views here.
 
+class RecipeFilter(filters.FilterSet):
+    categories = filters.BaseCSVFilter(label='categories', method='filter_categories')
+
+    class Meta:
+        model = models.Recipe
+        fields = ('title', 'categories',)
+
+    def filter_categories(self, qs, field_name, value, *args, **kwargs):
+        categories = models.Category.objects.filter(id__in=value)
+        for category in categories:
+            qs = qs.filter(
+                compositions__material__tags__id__in=category.requirements.all()
+            ).exclude(
+                compositions__material__tags__id__in=category.requirements.all()
+            )
+
+        qs = qs.distinct()
+        return qs
 
 
 class RecipeViewSet(FlexFieldsModelViewSet):
     queryset = models.Recipe.objects.all()
     serializer_class = serializers.RecipeSerializer
-    filterset_fields = ('title',)
+    # filterset_fields = ('title',)
+    filterset_class = RecipeFilter
     search_fields = ('title', 'description', 'instruction', 'tools')
 
     @action(detail=False, methods=['get',])
@@ -24,6 +48,27 @@ class RecipeViewSet(FlexFieldsModelViewSet):
         ).order_by('-datetime')
 
         return self.list(request)
+
+    @action(detail=True, methods=['post',])
+    def buy_ready(self, request, *args, **kwargs):
+        recipe = self.get_object()
+        compositions = recipe.compositions.all()
+        price = compositions.aggregate(
+            price=Sum(
+                F('material__price') * F('quantity')
+            )
+        )['price']
+
+        purchase = models.Purchase.objects.create(
+            recipe=recipe,
+            quantity=1,
+            price=price,
+            is_opportunity=True,
+            is_served=False
+        )
+
+        serializer = serializers.PurchaseSerializer(purchase)
+        return Response(serializer.data)
 
 
 class MaterialViewSet(FlexFieldsModelViewSet):
