8504f51639fc0b173657b4fe5c1d5b6879335be7
Reyvel <reyza@reyvel.id>
2019-08-30 14:10:23 +0700

    register admin

--- tokosehat/core/admin.py
+++ tokosehat/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.apps import apps
 
 # Register your models here.
+
+
+app = apps.get_app_config('core')
+models = app.get_models()
+
+
+for model in models:
+    admin.site.register(model)
